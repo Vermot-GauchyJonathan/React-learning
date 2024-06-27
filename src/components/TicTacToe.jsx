b69a6ed7a0930a738b17f772d75c3f11aaa69699
Jonathan Vermot-Gauchy <jonathan.vermot-gauchy@etu.univ-cotedazur.fr>
2024-06-27 11:30:09 +0200

    upgrade 2 done

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -81,27 +81,53 @@ function Square({ value, onSquareClick }) {
     } else {
       status = 'Prochain tour : ' + (xIsNext ? 'X' : 'O');
     }
-  
-    return (
-      <>
+    const boardRows = [];
+    const size = 3;
+
+    for (let i = 0; i < size; i++) {
+        const row = [];
+        for (let j = 0; j < size; j++) {
+        const squareIndex = i * size + j;
+        row.push(
+            <Square
+            key={squareIndex}
+            value={squares[squareIndex]}
+            onSquareClick={() => handleClick(squareIndex)}
+            />
+        );
+        }
+        boardRows.push(<div key={i} className="board-row">{row}</div>);
+    }
+
+  return (
+    <>
         <div className="status">{status}</div>
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
-      </>
-    );
+        <div className="board">{boardRows}</div>
+    </>
+    
+
+  );
+    //old
+    // return (
+    //   <>
+    //     <div className="status">{status}</div>
+    //     <div className="board-row">
+    //       <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+    //       <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+    //       <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+    //     </div>
+    //     <div className="board-row">
+    //       <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+    //       <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+    //       <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+    //     </div>
+    //     <div className="board-row">
+    //       <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+    //       <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+    //       <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+    //     </div>
+    //   </>
+    // );
   }
   
   
